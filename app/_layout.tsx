210d674f068ef0d70e67d9e83e75b71dad609c12
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2026-03-17 19:48:35 +0100

    refactor: migrate from RN Reusables to RN Paper

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,67 +1,60 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import { Pressable, Text } from 'react-native';
 import {
   MD3LightTheme as DefaultTheme,
+  IconButton,
   PaperProvider,
 } from 'react-native-paper';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import pantry from '@/app/pantry';
-import scanner from '@/app/scanner';
-import settings from '@/app/settings';
+
+import 'react-native-gesture-handler';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import PantryScreen from '@/app/index';
+import ScannerScreen from '@/app/scanner';
+import SettingsScreen from '@/app/settings';
+
+import Navbar from '@/components/navbar';
 
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
 } from 'expo-router';
 
-export default function RootLayout() {
-  const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator();
 
+export default function RootLayout() {
   return (
     <PaperProvider theme={DefaultTheme}>
-      <SafeAreaProvider>
-        <Stack.Navigator>
-          <Stack.Screen
-            name='Pantry'
-            component={pantry}
-            options={({ navigation }) => ({
-              title: 'Pantry',
-              headerLargeTitleEnabled: true,
-              headerSearchBarOptions: {
-                placeholder: 'Search products',
-              },
-              headerRight: () => (
-                <>
-                  <Pressable
-                    onPress={() => navigation.navigate('Scanner')}
-                    hitSlop={8}
-                  >
-                    <Text>Scan</Text>
-                  </Pressable>
-
-                  <Pressable
-                    onPress={() => navigation.navigate('Settings')}
-                    hitSlop={8}
-                  >
-                    <Text>Settings</Text>
-                  </Pressable>
-                </>
-              ),
-            })}
-          />
-          <Stack.Screen
-            name='Settings'
-            component={settings}
-            options={{ headerShown: true }}
-          />
-          <Stack.Screen
-            name='Scanner'
-            component={scanner}
-            options={{ presentation: 'modal', title: 'Scanner' }}
-          />
-        </Stack.Navigator>
-      </SafeAreaProvider>
+      <Stack.Navigator
+        initialRouteName='Pantry'
+        screenOptions={{
+          header: (props) => <Navbar {...props} />,
+        }}
+      >
+        <Stack.Screen
+          component={PantryScreen}
+          name='Pantry'
+          options={({ navigation }) => ({
+            title: 'Pantry',
+            headerRight: () => (
+              <>
+                <IconButton
+                  icon='barcode-scan'
+                  onPress={() => navigation.navigate('Scanner')}
+                />
+                <IconButton
+                  icon='cog-outline'
+                  onPress={() => navigation.navigate('Settings')}
+                />
+              </>
+            ),
+          })}
+        />
+        <Stack.Screen name='Settings' component={SettingsScreen} />
+        <Stack.Screen
+          name='Scanner'
+          component={ScannerScreen}
+          options={{ presentation: 'modal', title: 'Scanner' }}
+        />
+      </Stack.Navigator>
     </PaperProvider>
   );
 }
