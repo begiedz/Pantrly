3bc29588619e2fd9bd7f7875306085d11d7f23dd
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2026-03-29 18:38:20 +0200

    chore: clean appStore example data

--- src/store/appStore.ts
+++ src/store/appStore.ts
@@ -2,17 +2,7 @@ import { createStore } from '@tanstack/react-store';
 import type { AppStore, ProductEntity } from '@/types';
 
 export const appStore = createStore<AppStore>({
-  products: [
-    {
-      id: '1',
-      barcode: 'bardoce',
-      name: 'Name',
-      brand: 'Brand',
-      categories: ['dairy', 'beverage'],
-      imageUrl:
-        'https://fastly.picsum.photos/id/468/200/200.jpg?hmac=ebOvOZemklGsjJmYIRJ4_YWUDCNNpt5bE0B7EjYJfEA',
-    },
-  ],
+  products: [],
 });
 
 export function addProduct(product: ProductEntity) {
