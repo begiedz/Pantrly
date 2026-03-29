fd716492aa5a91edf1a7af102ebe56f917ff6b87
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2026-03-29 17:00:29 +0200

    feat: add pushing to store function

--- src/store/pantryStore.ts
+++ src/store/pantryStore.ts
@@ -1,6 +1,13 @@
 import { createStore } from '@tanstack/react-store';
-import type { PantryStore } from '@/types';
+import type { PantryStore, ProductEntity } from '@/types';
 
 export const pantryStore = createStore<PantryStore>({
   products: [],
 });
+
+export function addProduct(product: ProductEntity) {
+  pantryStore.setState(state => ({
+    ...state,
+    products: [...state.products, product],
+  }));
+}
