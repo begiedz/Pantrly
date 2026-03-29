3e99f6c7679bc42f80348f9ae0eb5e28478640be
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2026-03-29 17:57:29 +0200

    fix: description leftover

--- src/mappers/productMapper.ts
+++ src/mappers/productMapper.ts
@@ -11,7 +11,6 @@ export default function mapApiProductToEntity(
     id: crypto.randomUUID(),
     barcode: response.code,
     name: response.product.product_name,
-    description: response.product.compared_to_category,
     brand: response.product.brands_tags[0],
     categories: response.product.categories_tags ?? [],
     imageUrl: response.product.image_front_url,
