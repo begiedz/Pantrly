5161387f732029a7e626feefbb898e565c9276da
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2026-03-29 17:55:43 +0200

    feat: display pantry products from global store

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,35 +1,10 @@
+import { useStore } from '@tanstack/react-store';
 import { Link, router } from 'expo-router';
 import { FlatList, StyleSheet, Text, View } from 'react-native';
 import { FAB } from 'react-native-paper';
 import Card from '@/components/card';
 import Screen from '@/components/screen';
-
-const DATA = [
-  { id: '1', title: 'Item 1' },
-  { id: '2', title: 'Item 2' },
-  { id: '3', title: 'Item 3' },
-  { id: '4', title: 'Item 4' },
-  { id: '5', title: 'Item 5' },
-  { id: '6', title: 'Item 6' },
-  { id: '7', title: 'Item 7' },
-  { id: '8', title: 'Item 8' },
-  { id: '9', title: 'Item 9' },
-  { id: '10', title: 'Item 10' },
-  { id: '11', title: 'Item 11' },
-  { id: '12', title: 'Item 12' },
-  { id: '13', title: 'Item 13' },
-  { id: '14', title: 'Item 14' },
-  { id: '15', title: 'Item 15' },
-  { id: '16', title: 'Item 16' },
-  { id: '17', title: 'Item 17' },
-  { id: '18', title: 'Item 18' },
-  { id: '19', title: 'Item 19' },
-  { id: '20', title: 'Item 20' },
-  { id: '21', title: 'Item 21' },
-  { id: '22', title: 'Item 22' },
-  { id: '23', title: 'Item 23' },
-  { id: '24', title: 'Item 24' },
-];
+import { appStore } from '@/store/appStore';
 
 const Separator = () => <View style={{ height: 16 }} />;
 
@@ -49,14 +24,16 @@ const Welcome = () => (
 );
 
 export default function PantryScreen() {
+  const products = useStore(appStore, (state) => state.products);
+console.log('AppStore Products:',products);
   return (
     <Screen>
       <FlatList
         contentInsetAdjustmentBehavior='automatic'
         contentContainerStyle={{ margin: 16 }}
-        data={DATA}
-        renderItem={({ item }) => <Card item={item} />}
-        keyExtractor={(item) => item.id}
+        data={products}
+        renderItem={({item}) => <Card product={item} />}
+        keyExtractor={(product) => product.id}
         ItemSeparatorComponent={Separator}
         ListEmptyComponent={Welcome}
       />
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,26 +1,31 @@
 import { Avatar, type AvatarIconProps, Button, Card } from 'react-native-paper';
+import type { ProductEntity } from '@/types';
 
-const LeftContent = (props: AvatarIconProps) => (
-  <Avatar.Icon {...props} icon='folder' />
+const LeftContent = ({product, size }: {product:ProductEntity, size?: number }) => (
+  <Avatar.Image source={{uri:product.imageUrl}} size={size} />
 );
 
-const MyComponent = ({ item }: any) => (
-  <Card mode='contained'>
-    <Card.Title
-      title={item.title}
-      subtitle={`${item.id} item subtitle`}
-      left={LeftContent}
-    />
-    {/* <Card.Content>
+type PantryCardProps = {
+  product: ProductEntity;
+};
+
+export default function PantryCard({ product }: PantryCardProps) {
+  return (
+    <Card mode='contained'>
+      <Card.Title
+        title={product.name}
+        subtitle={product.brand}
+        left={({size})=> <LeftContent product={product} size={size}/>}
+      />
+      {/* <Card.Content>
       <Text variant='titleLarge'>Card title</Text>
       <Text variant='bodyMedium'>Card content</Text>
     </Card.Content>
     <Card.Cover source={{ uri: 'https://picsum.photos/700' }} /> */}
-    <Card.Actions>
-      <Button>Edit</Button>
-      <Button>Ok</Button>
-    </Card.Actions>
-  </Card>
-);
-
-export default MyComponent;
+      {/* <Card.Actions>
+        <Button>Edit</Button>
+        <Button>Ok</Button>
+      </Card.Actions> */}
+    </Card>
+  );
+}
